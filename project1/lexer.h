28418b3ab80a6e68b4079852f7c672516c5ed1af
ryanhe312 <ryanhe312@gmail.com>
2020-10-07 09:06:07 +0800

    lab1 init

---
+++ project1/lexer.h
@@ -0,0 +1,23 @@
+#ifndef _LEXER_H_
+#define _LEXER_H_
+
+#define ERROR       0x0
+#define KEYWORD     0x1
+#define OPERATOR    0x2
+#define DILIMITER   0x3
+
+#define INTEGER     0xff1
+#define REAL        0xff2
+#define ID          0xff3
+#define STRING      0xff4
+
+#define ILLEGAL_CHAR        0xffff0
+#define INVALID_INTEGER     0xffff1
+#define ILLEGAL_REAL        0xffff2
+#define ILLEGAL_ID          0xffff3
+#define ILLEGAL_STRING      0xffff4
+#define INVALID_STRING      0xffff5
+#define UNTERMINATED_STRING 0xffff6
+
+
+#endif
---
+++ project1/report.md
@@ -0,0 +1,30 @@
+## 格式
+
+标识符
+
+%%
+
+规则
+
+%%
+
+代码
+
+## 功能
+
+%option nounput
+
+不允许修改输入内容
+
+%option noyywrap
+
+不允许多个输入
+
+yylineno 行号
+yyset_in 更改输入
+yylex 解析
+yyget_leng 匹配长度
+yyget_text 匹配文字
+
+
+
